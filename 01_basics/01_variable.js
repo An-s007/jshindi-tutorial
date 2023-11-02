e3580fbf8088d4c1443526be65d37f7ecb6b495c
An-s007 <147226769+An-s007@users.noreply.github.com>
2023-11-02 05:45:48 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 14666
+let accountEmail = "anil@gmail.com"
+var accountPassword = "12345" 
+accountCity = "Jaipur"
+/*Do not use var variable*/
+accountEmail = "my@gmail.com"
+accountPassword = "4444"
+accountCity = "Surat"
+var accountState;
+// accountId = 1 // not allowed.
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
