2ac90303a91876d887320dc216bb2de41cdaf136
An-s007 <147226769+An-s007@users.noreply.github.com>
2023-11-02 05:45:24 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";// treat all js code as new version
+// alert( 3 + 3) // we are using nodejs, not browser
+console.log(3
+     + 3);  //code readability should be high
+
+     let name = "anil"
+     let age = 12
+     let isLoggedIn = false
+
+     // number => 2 to power 53
+     // bignit
+     // string =>
+     // null => standalone value
+     // boolean => true/false
+     // symbol => unique
+     
+
+     console.log(typeof "Anil");
+     console.log(typeof age);
+     console.log(typeof null);
+     console.log(typeof undefined);
+     console.log(typeof );
