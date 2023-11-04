19ae1ae9474f2f783efeda9893bd146cf6d25cd2
An-s007 <147226769+An-s007@users.noreply.github.com>
2023-11-04 17:48:14 +0000

    datatype summary

---
+++ 01_basics/05_datatype_summary.js
@@ -0,0 +1,16 @@
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+const heroes = ["anil", "sudani"];
+const myobj = {
+    name: "aniil", 
+    age: 22
+}
+console.log(heroes);
+console.log(myobj);
+
+const  myfunction = Function()
+{
+      console.log("hello world");
+}
