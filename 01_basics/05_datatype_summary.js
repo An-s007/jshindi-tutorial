490920e05a8f7cc0bed5f436065a4386e3c2acf8
An-s007 <147226769+An-s007@users.noreply.github.com>
2023-11-08 04:45:22 +0000

    stack and heap explanation

--- 01_basics/05_datatype_summary.js
+++ 01_basics/05_datatype_summary.js
@@ -1,7 +1,7 @@
 const id = Symbol('123')
 const anotherId = Symbol('123')
 console.log(id === anotherId);
-
+// const bigNumber = 4545345897458974587n converted into bigint      
 const heroes = ["anil", "sudani"];
 const myobj = {
     name: "aniil", 
@@ -13,4 +13,22 @@ console.log(myobj);
 const  myfunction = Function()
 {
       console.log("hello world");
+}   
+console.log(anotherId);
+
+// +++++++++++++++++++++++++++++
+    // Stack(Primitive), Heap(Non Primitive)
+
+    let myYoutubeName = "anilsudani01.com"
+    anotherName = myYoutubeName
+    anotherName = "Chaiaurcode"
+    console.log(anotherName);
+    console.log(myYoutubeName);
+let userOne = {     
+    email: "manish.com",
+    upi: "man@ibl"
 }
+let userTwo = userOne
+userTwo.email = "aaaa.com"
+console.log(userOne.email);
+console.log(userTwo.email);
